140cfb2b1b69b9192fa769b2295feb0b12cce1fb
Neeraj Web Developer <neerajhonda007@gmail.com>
2015-06-24 16:08:57 +0530

    elavator

---
+++ elevator/src/elevator/ElavatorFanButton.java
@@ -0,0 +1,16 @@
+package elevator;
+
+public class ElavatorFanButton {
+
+	public ElavatorFanButton() {
+		// TODO Auto-generated constructor stub
+	}
+
+	void clickOnButton(){
+		System.out.println("Button to turn On Fan");
+	}
+	
+	void clickOffButton(){
+		System.out.println("Button to turn Off Fan");
+	}
+}
---
+++ elevator/src/elevator/MainElavator.java
@@ -0,0 +1,18 @@
+package elevator;
+
+public class MainElavator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Elavator Program");
+		
+		ElavatorFanButton f = new ElavatorFanButton();
+
+		f.clickOnButton();
+		
+		
+
+		f.clickOffButton();
+	}
+
+}
